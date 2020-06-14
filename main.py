f6feef26720517b0a96bb256fe6b9575328581a3
Adrian Turtoczki <spysappin98@gmail.com>
2020-06-15 00:01:17 +0200

    Clicks cookie popup automatically now

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ URL = 'https://www.last.fm/user/'+user_name+'/listening-report/week'
 
 driver.get(URL)
 
+button = driver.find_element_by_id('onetrust-accept-btn-handler')
+button.click()
+
 S = lambda l: driver.execute_script('return document.body.parentNode.scroll'+l)
 driver.set_window_size(S('Width'),S('Height'))                                                                                                          
 driver.find_element_by_tag_name('body').screenshot('web_screenshot.png')
+
 
 driver.quit()

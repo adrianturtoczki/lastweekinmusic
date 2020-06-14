4a65946d879187632d7371910c8052993fd683fd
Adrian Turtoczki <spysappin98@gmail.com>
2020-06-14 23:53:48 +0200

    Does a full size screenshot
    need to fix the cookie thing

---
+++ main.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import sys
+
+user_name = sys.argv[1]
+
+options = webdriver.ChromeOptions()
+options.headless = True
+driver = webdriver.Chrome(options=options)
+
+URL = 'https://www.last.fm/user/'+user_name+'/listening-report/week'
+
+driver.get(URL)
+
+S = lambda l: driver.execute_script('return document.body.parentNode.scroll'+l)
+driver.set_window_size(S('Width'),S('Height'))                                                                                                          
+driver.find_element_by_tag_name('body').screenshot('web_screenshot.png')
+
+driver.quit()
